698d997a15b9247f1269a4e9eb5710e661afb769
s2nder <s2nder@gmail.com>
2024-11-12 14:03:34 -0500

    upload deployment

--- azure_func_git/terraform/main.tf
+++ azure_func_git/terraform/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "main" {
-  name     = "rg-${var.application_name}-${var.environment_name}"
+  name     = "myResourceGroup" #"rg-${var.application_name}-${var.environment_name}"
   #name     = var.resource_group_name
   location = var.location
 }
